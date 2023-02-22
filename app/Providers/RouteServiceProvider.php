cc1023a057e2db30221a74471fd3c50aae0ad0ec
zakieki55 <zakueki@gmail.com>
2023-02-22 21:59:03 +0700

    update

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -19,6 +19,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     public const HOME = '/home';
     public const REGISTER = '/register';
+    public const LOGIN = '/';
 
     /**
      * The controller namespace for the application.
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@include('layouts.sidebar')
+@include('layouts.sidebaradmin')
 @section('content')
 <html>
 
